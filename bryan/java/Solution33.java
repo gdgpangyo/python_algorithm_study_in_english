cefe21a67b237d6ccffbac2e7e3461f1100e9f8d
libliboom <lbsequ@gmail.com>
2020-04-23 21:01:27 +0900

    completed solution 33

---
+++ bryan/java/Solution33.java
@@ -0,0 +1,20 @@
+package leetcode.tiq.easy.java;
+
+import leetcode.tiq.easy.java.precondition.VersionControl;
+
+public class Solution33 extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
